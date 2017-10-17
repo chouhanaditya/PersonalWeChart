c732c705022dc86b4c29794cd95467683e7d8e33
Harsha <hverma@unomaha.edu>
2017-10-16 20:05:06 -0500

    10/16/2017 -Harsha Verma -adding view file for surgical history

---
+++ resources/views/patient/surgical_history.blade.php
@@ -0,0 +1,63 @@
+{{--Surgical History--}}
+<div class="container-fluid">
+    <div class="panel panel-default">
+        <div class="panel-heading" style="background-color: lightblue">
+            <b>Surgical History</b>
+        </div>
+        <div class="panel-body">
+            <div class="container-fluid">
+                <!-- Search For Diagnosis -->
+                <div class="row">
+                    <div class="col-md-3 col-md-offset-1">
+                        <label for="SearchForDiagnosis" > Search For Diagnosis:</label>
+                    </div>
+                    <div class="col-md-6">
+                        <input id="search_for_diagnosis_surgical_history" type="text" class="form-control" name="SearchForDiagnosis">
+                    </div>
+                </div>
+                <br>
+                <!-- List of Surgeries -->
+                <div class="row">
+                    <div class="col-md-9 col-md-offset-1">
+                            <table class="table table-striped table-bordered table-hover">
+                                <thead>
+                                <tr style="background-color: lightgrey">
+                                    <th>List of surgeries</th>
+                                    <th></th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                        <td><p>Diabetes</p></td>
+                                        <td style="text-align: right">
+                                            <a class="btn btn-danger" id="delete_surgical_history"> Delete</a>
+
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                    </div>
+                </div>
+                <br>
+                <!-- Comment box -->
+                <div class="row">
+                    <div class="col-md-3 col-md-offset-1">
+                        <label for="Comment"> Comments:</label>
+                    </div>
+                    <div class="col-md-6">
+                        <textarea rows="4" id="surgical_history_comment" style="width: 400px">
+                    </textarea>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-md-8" style="float: right">
+                    <button type="submit" id="btn_save_surgical_history" class="btn btn-primary">
+                        Save Surgical History
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
