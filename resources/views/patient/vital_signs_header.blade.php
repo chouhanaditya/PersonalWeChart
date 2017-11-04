77101b538187e43797ae1a4d783846d73d682be2
SweetyGoenka <sgoenka@unomaha.edu>
2017-11-03 23:56:39 -0500

    Sweety - 11/03 - Vital Signs

--- resources/views/patient/vital_signs_header.blade.php
+++ resources/views/patient/vital_signs_header.blade.php
@@ -1,59 +1,156 @@
-@section('content')
-    {{--@parent--}}
-<!--This is a container for vital signs header -->
-<div class="container-fluid" style="padding-top: 0;margin-top:0; padding-bottom: 0;margin-bottom: 0;">
-    <div class="row" >
-        <div class="panel-heading" style="padding-left: 0">
-            <a href="{{url('/StudentHome')}}" class="btn btn-success" style="float: left">
-                <i class="fa fa-arrow-circle-left" aria-hidden="true"></i>
-                Back to Dashboard
-            </a>
-            <h3 align="center" style="margin-top: 0;"><b>Patient Active Record</b></h3>
-        </div>
-        <div class="panel-body" style="margin-bottom: 0;padding-bottom: 0;background-color: #FFFAF0;margin-top: 0;padding-top: 0">
-            <table class="table" style=" margin-top: 0;padding-top: 0;margin-bottom: 0;padding-bottom: 0">
-                <!--This is the first row in the vital signs panel -->
-                <tr style=" margin-top: 0;padding-top: 0;">
-                    <td style="padding-left: 10%">
-                        <label >Name: {{$patient->first_name}} {{$patient->last_name}}</label>
-                    </td>
-                    <td style="padding-left: 10%">
-                        <label >Age: {{$patient->age}} </label>
-                      </td>
-                    <td style="padding-left: 10%">
-                        <label >Sex: {{$patient->gender}}</label>
-                    </td>
-                </tr>
+{{--@extends('layouts.app')--}}
+{{--@extends('patient.vital_signs_header')--}}
+@extends('patient.active_record')
 
-                <!--This is the second row in the vital signs panel -->
-                <tr >
-                    <td style="padding-left: 10%">
-                        <label >Blood Pressure(BP)Systolic: </label>
-                    </td>
-                    <td style="padding-left: 10%">
-                        <label >Respiratory Rate(RR):</label>
-                    </td>
-                    <td style="padding-left: 10%">
-                        <label >Temperature:</label>
-                    </td>
-                </tr>
-                <!--This is the third row in the vital signs panel -->
-                <tr >
-                <td style="padding-left: 10%">
-                    <label >Blood Pressure(BP)Diastolic: </label>
-                </td>
-                <td style="padding-left: 10%">
-                    <label >Heart Rate(HR): </label>
-                </td>
-                <td style="padding-left: 10%">
-                    <label >Pain: </label>
-                </td>
-            </tr>
-            </table>
+@section('documentation_panel')
+    {{--@parent--}}
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-14">
+                <div class="panel panel-default">
+                    <div class="panel-heading" style="background-color: lightblue;padding-bottom: 0">
+                        <h4 style="margin-top: 0" id="hpi_heading">Vital Signs</h4>
+                    </div>
+
+                    <div class="panel-body col-md-offset">
+                            <div class="col-md-30" style="float: right">
+                                <button type="submit" id="btn_add_vital_signs" class="btn btn-primary">
+                                    Add Vital Signs
+                                </button>
+                            </div>
+                            <br><br>
+                            <div class="row" style="overflow-x: scroll;width: 825px" class="col-md-12">
+                            <table class="table table-striped table-bordered table-hover" style="margin-top:10px; margin-left:15px;" id="vital_signs_table">
+                                <tr style="background: lightblue">
+                                    <th>Timestamp</th>
+                                    <th>BP Systolic</th>
+                                    <th>BP Diastolic</th>
+                                    <th>Heart Rate</th>
+                                    <th>Respiratory Rate</th>
+                                    <th>Temperature</th>
+                                    <th>Weight</th>
+                                    <th>Height</th>
+                                    <th>Pain</th>
+                                    <th>Oxygen Saturation</th>
+                                    <th>Comment</th>
+                                    <th>Action</th>
+                                </tr>
+                                <tr>
+                                @foreach ($vital_sign_details as $vs)
+                                    <tr>
+                                        <td>{{ $vs->timestamp }}</td>
+                                        <td>{{ $vs->BP_Systolic }}</td>
+                                        <td>{{$vs->BP_Diastolic}}</td>
+                                        <td>{{$vs->Heart_Rate}}</td>
+                                        <td>{{$vs->Respiratory_Rate}}</td>
+                                        <td>{{$vs->Temperature}}</td>
+                                        <td>{{$vs->Weight}}</td>
+                                        <td>{{$vs->Height}}</td>
+                                        <td>{{$vs->Pain}}</td>
+                                        <td>{{$vs->Oxygen_Saturation}}</td>
+                                        <td>{{$vs->Comment}}</td>
+                                        <td>
+                                            {{ Form::open(array('method' => 'post', 'route' => array('delete_vital_signs', $vs->timestamp))) }}
+                                                <input id="patient_id" name="patient_id" type="hidden" value="{{ $patient->patient_id }}">
+                                                <input type=hidden id="user_id" name="user_id" value="{{ Auth::user()->id }}">
+                                            <button name="delbutton" class="btn btn-danger btn-delete btn-sm">Delete</button>
+                                            {{ Form::close() }}</td>
+                                    </tr>
+                                    @endforeach
+                                </tr>
+
+                            </table>
+                            </div>
+                            <br><br>
+
+                            <div class="row" style="overflow-x: scroll;width: 825px" class="col-md-12">
+                                <form class="form-horizontal" method="POST" action="{{ url('post_vital_signs') }}">
+                                    {{ csrf_field() }}
+                                    <input id="patient_id" name="patient_id" type="hidden" value="{{ $patient->patient_id }}">
+                                    <input type=hidden id="user_id" name="user_id" value="{{ Auth::user()->id }}">
+                                    <!-- <input id="timestamp" name="timestamp" type="hidden"> -->
+                            <table class="table table-striped table-bordered table-hover" id="table_child_vital_signs" style="margin-top:10px; margin-left:15px;">
+                                <tr style="background: lightblue">
+                                    <th>BP Systolic</th>
+                                    <th>BP Diastolic</th>
+                                    <th>Heart Rate</th>
+                                    <th>Respiratory Rate</th>
+                                    <th>Temperature</th>
+                                    <th>Weight</th>
+                                    <th>Height</th>
+                                    <th>Pain</th>
+                                    <th>Oxygen Saturation</th>
+                                    <th>Comment</th>
+                                    <th>Action</th>
+                                </tr>
+
+                                <tr>
+                                    <td>
+                                        <input type="text" name="BP_Systolic" id="BP_Systolic" style="width: 100px;">
+                                    </td>
+                                    <td>
+                                        <input type="text" name="BP_Diastolic" id="BP_Diastolic" style="width: 100px;">
+                                    </td>
+                                    <td>
+                                        <input type="text" name="Heart_Rate" id="Heart_Rate" style="width: 100px;">
+                                    </td>
+                                    <td>
+                                        <input type="text" name="Respiratory_Rate" id="Respiratory_Rate" style="width: 100px;">
+                                    </td>
+                                    <td>
+                                        <input type="text" name="Temperature" id="Temperature" style="width: 100px;">
+                                        <select name="temperature_unit" id = "temperature_unit">
+                                            <option value="F">F</option>
+                                            <option value="C">C</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <input type="text" name="Weight" id="Weight" style="width: 100px;">
+                                        <select name="weight_unit" id = "weight_unit">
+                                            <option value="kgs">kgs</option>
+                                            <option value="lbs">lbs</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <input type="text" name="Height" id="Height" style="width: 100px;">
+                                        <select name="height_unit" id="height_unit">
+                                            <option value="cms">cms</option>
+                                            <option value="inches">inches</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <input type="text" name="Pain" id="Pain" style="width: 100px;">
+                                    </td>
+                                    <td>
+                                        <input type="text" name="Oxygen_Saturation" id="Oxygen_Saturation" style="width: 100px;">
+                                    </td>
+                                    <td>
+                                        <input type="text" name="Comments" id="Comments" style="width: 100px;">
+                                        </td>
+                                    <td>
+                                        <button name="submitbutton" class="btn btn-success btn-submit btn-sm">Add</button>
+                                    </td>
+                                </tr>
+
+                            </table>
+                                </form>
+                            </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="row" style="padding-bottom: 0;margin-bottom: 0">
-        @yield('Maincontent')
-    </div>
-</div>
+
+    <script>
+        $(document).ready(function(){
+            $('#table_child_vital_signs').hide();
+            $("#btn_add_vital_signs").click(function(){
+                //$('#onetimedisplay').hide();
+                $('#table_child_vital_signs').show();
+                $("#btn_add_vital_signs").hide();
+            });
+        });
+       
+    </script>
+
 @endsection
